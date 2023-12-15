b82336bfd5ea2d2e75ada8cf1165d3f29a10b949
JD Flipando <juandiegobalbi@flipando.ai>
2023-12-15 16:41:06 -0300

    add complete term sheet content

--- src/term_sheet.py
+++ src/term_sheet.py
@@ -65,4 +65,120 @@ shares of preferred stock or common stock and (h) taking any of the foregoing ac
 Company. 
 
 
+B. Terms of Financing Agreements
+- Representations and Warranties: Standard representations and warranties by the Company. 
+
+- Conditions to Closing: 
+    (a) Standard conditions to closing, which shall include, among other things, satisfactory completion of financial, intellectual property and legal due diligence by the Investors.
+    (b) Investors shall have received a customary opinion of counsel for the Company.
+    (c) The Founders and all current and former employees and consultants shall have entered into the Company's or its
+subsidiary's standard   form   proprietary   information   and   inventions   agreement   in   form   and   substance   acceptable   to
+Inversores Cracks.
+    (d) Board composition at closing shall be as described under “Board of Directors” below.
+    (e) The Company shall have entered into indemnification agreements with the members of the Board in a form acceptable to Inversores Cracks.
+    (f) Counsel to  Inversores Cracks  will draft documents. The Company shall pay, at the closing, reasonable fees and expenses of Inversores Cracks's counsel, not to exceed US$35,000.   
+
+- Founder and Employee Vesting: The Founders' common stock shall vest as follows: after 12 months of services following the initial closing, 33.33% will
+vest; the remainder will vest monthly over the following 24 months. Unless otherwise approved by the Board, employees' common stock shall vest as follows: after 12 months of employment, 25% will vest; the remainder will vest monthly over
+the following 36 months.  The Company shall have a repurchase option on unvested stock at cost.
+
+- Right of First Refusal and Co-Sale Right:
+Until the IPO, the Major Investors (as defined below) shall have the right to participate on a pro rata basis in transfers of
+any shares held by the Founders, and a right of first refusal on such transfers, subordinate to the Company's right of first
+refusal. Any shares not subscribed for by an Investor may be reallocated pro rata among the other eligible Investors. 
+The right of first refusal and co-sale shall not apply, in the case of an individual, to (a) transfers to any spouse or member
+of Founder's immediate family, or to other estate planning transfers or (b) any sale to the public pursuant to an effective
+registration, and in the case of an entity, to an affiliate. 
+
+- Drag-Along Right: In the event of a sale of the Company by way of a merger, recapitalization, sale of all or substantially all of the Company's
+stock or assets or otherwise, which is approved by (a) the Board (including the Series A Director) and (b) the Requisite
+Holders, the Investors, the Founders and the Company's other stockholders shall be required to vote for, consent to,
+transfer their stock pursuant to, and take such other action as may be required to consummate, such sale, subject to
+customary conditions and exceptions; provided that the aggregate purchase price for such sale (including any contingent
+amounts) is at least 4x the post-money valuation of the Company after the Series A financing.
+
+- Pro Rata Rights/Right of First Offer: Each Investor holding at least 20% outstanding shares of Series A Preferred Stock (the “
+pro rata right, but not an obligation, based on their percentage equity ownership of common stock, assuming full
+conversion and exercise of all outstanding convertible and exercisable securities, to participate in subsequent financings of
+the Company, subject to customary exceptions. Any securities not subscribed for by a Major Investor may be reallocated
+pro-rata among the other eligible Major Investors. Such right shall not apply to any public offering and will terminate
+immediately prior to an IPO.
+
+- Registration Rights: Customary registration rights including (a) two demand 
+closing or six months after the Company's IPO; (b) unlimited piggyback rights, subject to 
+unlimited S-3 registrations, subject to customary limitations; in all cases, expenses are to be paid by the Company,
+including counsel for the selling stockholders.
+
+- Financial Information: The Major Investors will have customary information and inspection rights. 
+
+- Matters Requiring Series A Director Approval: The Company will not, and will not permit a subsidiary of the Company to, without Board approval, which approval must
+include the affirmative vote or consent of the Series A Director (as defined below):
+    (a) lease (in excess of an amount equal to US$100,000 annually), purchase, or acquire any real property; 
+    (b) acquire, sell, assign or transfer any equity in any subsidiary (other than to another group company); 
+    (c) grant options to purchase, or the right to subscribe for, common stock of the Company, if the shares of common stock subject of any such grant represent
+1% or more of the fully-diluted capitalization of the Company; 
+    (d) hire, fire or set the compensation of the officers of the Company or a subsidiary; 
+    (e) establish any bonus payment to any of the executive officers of the Company or of any subsidiary;
+    (f) initiate dissolution proceedings of the Company or a subsidiary;
+    (g) enter into any related-party transactions; 
+    (h) dispose of intellectual property of the Company or a subsidiary; 
+    (i) grant any loans to any individual or entity, except for loans to the Company or another subsidiary; 
+    (j) incur any indebtedness in excess of an amount equal to US$100,000, not contemplated in the annual budget of the Company; (k) sell, lease or dispose any assets of the Company
+or any subsidiary, with a value in excess of US$100,000, not contemplated in the annual budget of the Company; 
+    (l) enter into any commercial agreement, in excess of US$100,000, not contemplated in the annual budget of the Company;
+    (m) initiate any legal proceeding involving amounts in excess of US$100,000 and (n) approve the Company's annual business plan.
+
+- Founders' Activity: Each Founder shall devote 100% of his/her professional time to the Company. Any other professional activity (other than
+serving as independent directors of other entities that are not competitors of the Company) will require the approval of the
+Board of Directors (including the Series A Director (as defined below)).
+
+- Management Rights Letter: Inversores Cracks shall receive a standard management rights letter providing for the consultation with management on
+significant issues, access to the books, records and facilities of the Company, and Board visitation rights.
+
+
+C. Board of Directors Matters
+- Board of Directors: At the initial closing, the Board shall consist of three members.  Holders of Series A Preferred Stock shall be entitled to
+elect one member (the “Series A Director”), who shall be designated by  shall be entitled to elect two members, who shall be designated by the Founders who are then providing services to the
+Company or any of its subsidiaries as officers, employees or consultants.
+
+D. Other Relevant Matters
+- Tax Matters: The Company will provide standard covenants with respect to the Company's status as a controlled foreign corporation
+and passive foreign investment company.
+The Company will use commercially reasonable efforts to conduct its and its subsidiaries' affairs so that the Investors and
+their equity holders are not, for U.S. tax purposes, deemed to have unrelated business taxable income, to be engaged in a
+trade or business within the United States or to recognize income that is effectively connected with a United States trade
+or business.
+
+- Directors and Officers Liability Insurance: Within 30 days of the initial closing, the Company shall obtain a directors and officers liability insurance policy in the
+amount and with terms acceptable to the Board (including the Series A Director).
+
+- Publicity: Except as required by law, the Company will not discuss the terms of this memorandum of terms with any person other
+than key officers, members of the Board of the Company or the Company's accountants or attorneys without the written
+consent of Inversores Cracks.  In addition, the Company shall not use an Investor's name in any manner, context or format
+(including reference on or links to websites, press releases, etc.) without the prior approval of such Investor.
+
+- No Shop: From the signing date hereof until the earlier of (i) ninety (90) days from the date of this memorandum of terms, (ii) the
+initial closing or (iii) written notification by Inversores Cracks
+Company agrees that it shall not solicit, encourage others to solicit, encourage or accept any offers for the purchase or
+acquisition of any stock of, or all or any substantial part of the assets of, the Company or proposals for any merger or
+consolidation involving the Company, and it shall not negotiate with or enter into any agreement or understanding with
+any other person with respect to any such transaction.
+
+- Governing Law: This term sheet shall be governed in all respects by the laws of the State of New York.
+
+
+Except as set forth below, this term sheet is non-binding and is intended solely as a summary of the
+terms that are currently proposed by the parties.   The parties acknowledge that they neither intend to
+enter, nor have they entered into, any agreement to negotiate a definitive agreement pursuant to this
+memorandum of terms, and either party may, at any time prior to execution of such definitive agreement,
+propose different terms from those summarized herein or unilaterally terminate all negotiations pursuant
+to this memorandum of terms without any liability whatsoever to the other party other than in regards to
+the paragraphs titled “Publicity,” “No Shop,” and “Governing Law,” which the parties understand and
+acknowledge   shall   be   binding.   Except   as   otherwise   provided   in   item   (f)   of   the   paragraph   titled
+“Conditions to Closing,” each party shall be solely liable for all of its own fees, costs and other expenses
+in conjunction with negotiation and preparation of a final agreement pursuant to this term sheet.
+If the terms of this memorandum are acceptable to the Company, please so indicate on the enclosed
+copy of this memorandum and return it to the undersigned no later than 5:00 P.M. Eastern Standard Time
+on [____________], 2023.
+
 """
