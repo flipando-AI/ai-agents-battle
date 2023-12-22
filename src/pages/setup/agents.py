ee1f0aa35a280550e380a5d7df1a285bd9f70fb3
JD Flipando <juandiegobalbi@flipando.ai>
2023-12-22 15:46:59 -0300

    change default agent name

--- src/pages/setup/agents.py
+++ src/pages/setup/agents.py
@@ -8,7 +8,7 @@ from src.battle.prompts import (system_message_analytics_assistant,
 
 def init_default_agents():
     return [
-        ("Laurie", system_message_shark_partner, "gpt-35-turbo-16k", False, "Right"),
+        ("Willy", system_message_shark_partner, "gpt-35-turbo-16k", False, "Right"),
         ("Monica", system_message_analytics_assistant, "gpt-35-turbo-16k", False, "Right"),
         ("Flipper", system_message_confident_founder, "gpt-35-turbo-16k", False, "Left"),
         ("Dwight", system_message_shy_founder, "gpt-35-turbo-16k", False, "Left")
